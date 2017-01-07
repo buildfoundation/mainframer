91b5d72ee35eb52bef0caac330eb6ec5664fb869
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-01-07 04:32:03 +0300

    Add test to check that local files were not deleted.

---
+++ test/test_keeps_files_on_local_machine.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# You can run it from any directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Execute common pre-setup, include test functions.
+source "$DIR/common.sh"
+
+printTestStarted
+
+# Create several files that should be synced to remote machine AND KEEPT after execution.
+mkdir "$BUILD_DIR/src"
+touch "$BUILD_DIR/src/file1.txt"
+touch "$BUILD_DIR/src/file2.txt"
+touch "$BUILD_DIR/src/file3.txt"
+
+# Run mainframer.sh that noops.
+bash "$BUILD_DIR"/mainframer.sh 'echo noop'
+
+# Make sure files STILL exist on local machine after execution.
+fileMustExistOnLocalMachine "src/file1.txt" "(sync problem)"
+fileMustExistOnLocalMachine "src/file2.txt" "(sync problem)"
+fileMustExistOnLocalMachine "src/file3.txt" "(sync problem)"
+
+printTestEnded
