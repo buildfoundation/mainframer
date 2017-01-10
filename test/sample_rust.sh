86b85d2763014b0d5a1cec410c6e40af333a314b
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-01-11 01:08:55 +0300

    Add rust sample project.

---
+++ test/sample_rust.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# You can run it from any directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Execute common pre-setup, include test functions.
+source "$DIR/common.sh"
+
+printTestStarted
+
+# Copy Rust sample to build dir.
+cp -a "$DIR/../samples/rust/." "$BUILD_DIR"
+
+# Overwrite config to work with test remote machine.
+setTestRemoteMachineInPersonalConfig
+
+# Run mainframer.sh that builds Rust project.
+bash "$BUILD_DIR"/mainframer.sh 'cargo build'
+
+# Run binary to ensure that it was built fine.
+eval "$BUILD_DIR/target/debug/mainframer_rust_sample"
+
+printTestEnded
