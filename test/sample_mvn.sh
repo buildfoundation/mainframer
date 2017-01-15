57223f3e33b0ce233a7b18aac673cc0882a1ccda
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-01-15 20:47:59 +0300

    Add Maven sample.

---
+++ test/sample_mvn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# You can run it from any directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Execute common pre-setup, include test functions.
+source "$DIR/common.sh"
+
+printTestStarted
+
+# Copy Maven sample to build dir.
+cp -a "$DIR/../samples/mvn/." "$BUILD_DIR"
+
+# Overwrite config to work with test remote machine.
+setTestRemoteMachineInConfig
+
+# Run mainframer.sh that builds Maven project.
+bash "$REPO_DIR"/mainframer.sh './mvnw clean package'
+
+# Run jar to ensure that it was built fine.
+java -jar "$BUILD_DIR/target/sample-1.0.jar"
+
+printTestEnded
