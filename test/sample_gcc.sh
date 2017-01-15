a36cfc19501a6c7788993ad833d537be912a31a2
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-01-15 22:10:43 +0300

    Add GCC sample.

---
+++ test/sample_gcc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# You can run it from any directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Execute common pre-setup, include test functions.
+source "$DIR/common.sh"
+
+printTestStarted
+
+# Copy GCC sample to build dir.
+cp -a "$DIR/../samples/gcc/." "$BUILD_DIR"
+
+# Overwrite config to work with test remote machine.
+setTestRemoteMachineInConfig
+
+# Run mainframer.sh that builds GCC project.
+bash "$REPO_DIR"/mainframer.sh 'gcc -Wall sample.c -o sample'
+
+# Run binary to ensure that it was built fine.
+eval "$BUILD_DIR/sample"
+
+printTestEnded
