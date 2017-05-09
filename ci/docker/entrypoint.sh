f37d3f026f98fb42d2c6b46218e263b345a6aaa5
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-05-09 13:47:14 +0300

    WIP: Dockerize CI.

---
+++ ci/docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# See https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
+
+# Add local user.
+# Either use the LOCAL_USER_ID if passed in at runtime or fallback to 9001.
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "Starting with UID : $USER_ID"
+groupadd --gid $USER_ID build_user
+useradd --shell /bin/bash --uid $USER_ID --gid $USER_ID --comment "User for container" --create-home build_user
+
+# Grant build_user access to Android SDK.
+chown -R build_user:build_user $ANDROID_HOME
+
+# Run original docker run command as build_user.
+sudo --set-home --preserve-env -u build_user "$@"
