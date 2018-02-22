e718688a8eaf50c6ce6f90c2ebc36b97ad1b69ae
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2018-02-22 04:29:13 -0800

    Add tests for Args.

--- src/args.rs
+++ src/args.rs
@@ -1,12 +1,12 @@
+#[derive(Debug, PartialEq, Eq)]
 pub struct Args {
     command: String
 }
 
 impl Args {
-
     pub fn parse(raw_args: Vec<String>) -> Result<Args, String> {
         if raw_args.len() == 0 {
-            return Err(String::from("Please pass remote command.")); // TODO more user friendly message.
+            return Err(String::from("Please pass remote command.")); // TODO more user friendly message, for now it's consistent with Bash version.
         } else if raw_args.len() > 1 {
             return Err(String::from("Mainframer supports only 1 argument which is a command that need to be executed."));
         } else {
@@ -17,3 +17,25 @@ impl Args {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_ok() {
+        let raw_args = vec![String::from("test command")];
+        assert_eq!(Args::parse(raw_args), Ok(Args { command: String::from("test command") }));
+    }
+
+    #[test]
+    fn parse_empty() {
+        let raw_args: Vec<String> = vec![];
+        assert_eq!(Args::parse(raw_args), Err(String::from("Please pass remote command.")));
+    }
+
+    #[test]
+    fn parse_more_than_1_arg() {
+        let raw_args = vec![String::from("test"), String::from("command")];
+        assert_eq!(Args::parse(raw_args), Err(String::from("Mainframer supports only 1 argument which is a command that need to be executed.")));
+    }
+}
