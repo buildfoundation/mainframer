aa3b0621aa15dc8ea2adbd0002d49a49239df6df
Artem Zinnatullin <ceo@artemzin.com>
2018-11-25 23:17:29 -0800

    More tests

--- src/intermediate_config.rs
+++ src/intermediate_config.rs
@@ -29,12 +29,12 @@ impl IntermediateConfig {
         let mut content = String::new();
 
         let mut file = match File::open(file_path) {
-            Err(_) => return Err(format!("Could not open config file '{}'.", file_path.to_string_lossy())),
+            Err(_) => return Err(format!("Could not open config file '{}'", file_path.to_string_lossy())),
             Ok(value) => value,
         };
 
         file.read_to_string(&mut content)
-            .unwrap_or_else(|_| panic!("Could not read config file '{}'.", file_path.to_string_lossy()));
+            .unwrap_or_else(|_| panic!("Could not read config file '{}'", file_path.to_string_lossy()));
 
         match parse_config_from_str(&content) {
             Err(message) => Err(format!("Error during parsing config file '{}'\n{}", file_path.to_string_lossy(), message)),
@@ -45,11 +45,9 @@ impl IntermediateConfig {
 
 fn parse_config_from_str(config_content: &str) -> Result<IntermediateConfig, String> {
     let yaml = match YamlLoader::load_from_str(config_content) {
-        Err(error) => return Err(format!("Error during parsing of config.yml: {:#?}", error)),
+        Err(error) => return Err(format!("YAML parsing error {:#?}", error)),
         Ok(content) => content[0].to_owned()
     };
-
-    println!("Yaml: {:#?}", yaml);
 
     let remote_machine = match &yaml["remoteMachine"] {
         Yaml::Hash(remote_machine) => {
@@ -57,7 +55,7 @@ fn parse_config_from_str(config_content: &str) -> Result<IntermediateConfig, Str
                 Some(host) => match host {
                     Yaml::String(host) => Some(host.to_string()),
                     Yaml::Null => None,
-                    _ => return Err(String::from("Error during parsing of config.yml, remoteMachine.host must be a string."))
+                    _ => return Err(String::from("remoteMachine.host must be a string"))
                 },
                 None => None
             };
@@ -66,7 +64,7 @@ fn parse_config_from_str(config_content: &str) -> Result<IntermediateConfig, Str
                 Some(user) => match user {
                     Yaml::String(user) => Some(user.to_string()),
                     Yaml::Null => None,
-                    _ => return Err(String::from("Error during parsing of config.yml, remoteMachine.user must be a string."))
+                    _ => return Err(String::from("remoteMachine.user must be a string"))
                 },
                 None => None
             };
@@ -80,30 +78,29 @@ fn parse_config_from_str(config_content: &str) -> Result<IntermediateConfig, Str
         ref something_else => return Err(format!("'remoteMachine' must be an object, but was {:#?}", something_else))
     };
 
-
     let compression = match &yaml["compression"] {
         Yaml::Hash(compression) => {
-            let local = match compression.get(&Yaml::String(String::from("local"))) {
+            let local = match compression.get(&Yaml::String(String::from("local"))).cloned() {
                 Some(local) => match local {
-                    Yaml::Integer(local) => if local >= &1 && local <= &9 {
-                        Some(local.to_owned())
+                    Yaml::Integer(local) => if local >= 1 && local <= 9 {
+                        Some(local)
                     } else {
                         return Err(format!("'compression.local' must be a positive integer from 1 to 9, but was {:#?}", local));
                     },
-                    Yaml::Null => None,
+                    Yaml::Null | Yaml::BadValue => None,
                     ref something_else => return Err(format!("'compression.local' must be a positive integer from 1 to 9, but was {:#?}", something_else))
                 },
                 None => None
             };
 
-            let remote = match compression.get(&Yaml::String(String::from("remote"))) {
+            let remote = match compression.get(&Yaml::String(String::from("remote"))).cloned() {
                 Some(remote) => match remote {
-                    Yaml::Integer(remote) => if remote >= &1 && remote <= &9 {
-                        Some(remote.to_owned())
+                    Yaml::Integer(remote) => if remote >= 1 && remote <= 9 {
+                        Some(remote)
                     } else {
                         return Err(format!("'compression.remote' must be a positive integer from 1 to 9, but was {:#?}", remote));
                     },
-                    Yaml::Null => None,
+                    Yaml::Null | Yaml::BadValue => None,
                     ref something_else => return Err(format!("'compression.remote' must be a positive integer from 1 to 9, but was {:#?}", something_else))
                 },
                 None => None
@@ -115,7 +112,7 @@ fn parse_config_from_str(config_content: &str) -> Result<IntermediateConfig, Str
             })
         }
         Yaml::Null | Yaml::BadValue => None,
-        _ => return Err(String::from("'compression' must be an object."))
+        _ => return Err(String::from("'compression' must be an object"))
     };
 
     Ok(IntermediateConfig {
@@ -129,7 +126,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_config_from_str_all_fields() {
+    fn parse_config_from_str_all_fields_2_spaces_indent() {
         let content = "
 remoteMachine:
   host: computer1
@@ -137,6 +134,28 @@ remoteMachine:
 compression:
   local: 5
   remote: 2"
+        ;
+        assert_eq!(parse_config_from_str(content), Ok(IntermediateConfig {
+            remote_machine: Some(IntermediateRemoteMachine {
+                host: Some(String::from("computer1")),
+                user: Some(String::from("user1")),
+            }),
+            compression: Some(IntermediateCompression {
+                local: Some(5),
+                remote: Some(2),
+            }),
+        }));
+    }
+
+    #[test]
+    fn parse_config_from_str_all_fields_4_spaces_indent() {
+        let content = "
+remoteMachine:
+    host: computer1
+    user: user1
+compression:
+    local: 5
+    remote: 2"
         ;
         assert_eq!(parse_config_from_str(content), Ok(IntermediateConfig {
             remote_machine: Some(IntermediateRemoteMachine {
@@ -208,6 +227,59 @@ compression:
                 remote: Some(2),
             }),
         }));
+    }
+
+    #[test]
+    fn parse_config_from_str_compression_valid_range() {
+        let mut compression_types: Vec<String> = Vec::new();
+
+        compression_types.push(String::from("local"));
+        compression_types.push(String::from("remote"));
+
+        for compression_type in compression_types {
+            for compression_level in 1..9 {
+                let content = format!("
+compression:
+  {:#?}: {:#?}
+", compression_type, compression_level);
+
+                assert_eq!(parse_config_from_str(&content), Ok(IntermediateConfig {
+                    remote_machine: None,
+                    compression: Some(IntermediateCompression {
+                        local: if compression_type == "local" { Some(compression_level) } else { None },
+                        remote: if compression_type == "remote" { Some(compression_level) } else { None },
+                    }),
+                }));
+            }
+        }
+    }
+
+    #[test]
+    fn parse_config_from_str_compression_invalid_range() {
+        let mut compression_types: Vec<String> = Vec::new();
+
+        compression_types.push(String::from("local"));
+        compression_types.push(String::from("remote"));
+
+        let mut invalid_compression_levels: Vec<i64> = Vec::new();
+
+        invalid_compression_levels.push(0);
+        invalid_compression_levels.push(10);
+        invalid_compression_levels.push(-1);
+
+        for compression_type in compression_types {
+            for compression_level in &invalid_compression_levels {
+                let content = format!("
+compression:
+  {:#?}: {:#?}
+", compression_type, compression_level);
+
+                assert_eq!(
+                    parse_config_from_str(&content),
+                    Err(format!("'compression.{}' must be a positive integer from 1 to 9, but was {}", compression_type, compression_level))
+                );
+            }
+        }
     }
 
     #[test]
