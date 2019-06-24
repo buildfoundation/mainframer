1f7c49bd9d9c072cc919b4847ecd03bb8453ebea
Artem Zinnatullin :slowpoke <ceo@artemzin.com>
2019-06-24 15:34:16 -0700

    Add millis to format_duration (#259)
    * Add millis to format_duration
    
    * Show previous time unit to major one, update rust

--- src/time.rs
+++ src/time.rs
@@ -1,4 +1,3 @@
-use std::fmt::Write;
 use std::time::Duration;
 
 pub fn format_duration(duration: Duration) -> String {
@@ -7,47 +6,40 @@ pub fn format_duration(duration: Duration) -> String {
     let hours = total_seconds / 3600;
     let minutes = (total_seconds % 3600) / 60;
     let seconds = total_seconds % 60;
-
-    let hours_label = match hours {
-        1 => "hour",
-        _ => "hours"
-    };
-
-    let minutes_label = match minutes {
-        1 => "minute",
-        _ => "minutes"
-    };
-
-    let seconds_label = match seconds {
-        1 => "second",
-        _ => "seconds"
-    };
-
-    let mut text = String::new();
+    let millis = (duration.as_millis() - (u128::from(total_seconds) * 1000)) % 1000;
 
     if hours > 0 {
-        push_value_and_label(&mut text, hours, hours_label);
+        format!("{} {} {} {}", hours, hours_label(hours), minutes, minutes_label(minutes))
+    } else if minutes > 0 {
+        format!("{} {} {} {}", minutes, minutes_label(minutes), seconds, seconds_label(minutes, seconds))
+    } else {
+        format!("{}.{:03} {}", seconds, millis, seconds_label(minutes, seconds))
     }
-
-    if minutes > 0 {
-        if !text.is_empty() {
-            text.push(' ');
-        }
-
-        push_value_and_label(&mut text, minutes, minutes_label);
-    }
-
-    if !text.is_empty() {
-        text.push(' ');
-    }
-
-    push_value_and_label(&mut text, seconds, seconds_label);
-
-    text
 }
 
-fn push_value_and_label(text: &mut String, value: u64, label: &str) {
-    write!(text, "{} {}", value, label).unwrap();
+fn hours_label(hours: u64) -> String {
+    match hours {
+        1 => "hour",
+        _ => "hours"
+    }.to_owned()
+}
+
+fn minutes_label(minutes: u64) -> String {
+    match minutes {
+        1 => "minute",
+        _ => "minutes"
+    }.to_owned()
+}
+
+fn seconds_label(minutes: u64, seconds: u64) -> String {
+    if minutes > 0 {
+        match seconds {
+            1 => "second",
+            _ => "seconds"
+        }
+    } else {
+        "seconds"
+    }.to_owned()
 }
 
 #[cfg(test)]
@@ -56,17 +48,27 @@ mod tests {
 
     #[test]
     fn format_duration_0_seconds() {
-        assert_eq!(format_duration(Duration::from_secs(0)), "0 seconds");
+        assert_eq!(format_duration(Duration::from_secs(0)), "0.000 seconds");
+    }
+
+    #[test]
+    fn format_duration_0_5_seconds() {
+        assert_eq!(format_duration(Duration::from_millis(500)), "0.500 seconds");
     }
 
     #[test]
     fn format_duration_1_second() {
-        assert_eq!(format_duration(Duration::from_secs(1)), "1 second");
+        assert_eq!(format_duration(Duration::from_secs(1)), "1.000 seconds");
+    }
+
+    #[test]
+    fn format_duration_1_5_second() {
+        assert_eq!(format_duration(Duration::from_millis(1500)), "1.500 seconds");
     }
 
     #[test]
     fn format_duration_2_seconds() {
-        assert_eq!(format_duration(Duration::from_secs(2)), "2 seconds");
+        assert_eq!(format_duration(Duration::from_secs(2)), "2.000 seconds");
     }
 
     #[test]
@@ -77,6 +79,11 @@ mod tests {
     #[test]
     fn format_duration_1_minute_1_second() {
         assert_eq!(format_duration(Duration::from_secs(61)), "1 minute 1 second");
+    }
+
+    #[test]
+    fn format_duration_1_minute_1_5_second() {
+        assert_eq!(format_duration(Duration::from_millis(61500)), "1 minute 1 second");
     }
 
     #[test]
@@ -101,46 +108,56 @@ mod tests {
 
     #[test]
     fn format_duration_1_hour() {
-        assert_eq!(format_duration(Duration::from_secs(3600)), "1 hour 0 seconds");
+        assert_eq!(format_duration(Duration::from_secs(3600)), "1 hour 0 minutes");
     }
 
     #[test]
     fn format_duration_1_hour_1_second() {
-        assert_eq!(format_duration(Duration::from_secs(3601)), "1 hour 1 second");
+        assert_eq!(format_duration(Duration::from_secs(3601)), "1 hour 0 minutes");
+    }
+
+    #[test]
+    fn format_duration_1_hour_1_5_second() {
+        assert_eq!(format_duration(Duration::from_millis(3601 * 1000 + 500)), "1 hour 0 minutes");
     }
 
     #[test]
     fn format_duration_1_hour_2_seconds() {
-        assert_eq!(format_duration(Duration::from_secs(3602)), "1 hour 2 seconds");
+        assert_eq!(format_duration(Duration::from_secs(3602)), "1 hour 0 minutes");
     }
 
     #[test]
     fn format_duration_1_hour_1_minute() {
-        assert_eq!(format_duration(Duration::from_secs(3660)), "1 hour 1 minute 0 seconds");
+        assert_eq!(format_duration(Duration::from_secs(3660)), "1 hour 1 minute");
     }
 
     #[test]
     fn format_duration_1_hour_1_minute_1_second() {
-        assert_eq!(format_duration(Duration::from_secs(3661)), "1 hour 1 minute 1 second");
+        assert_eq!(format_duration(Duration::from_secs(3661)), "1 hour 1 minute");
+    }
+
+    #[test]
+    fn format_duration_1_hour_1_minute_1_5_second() {
+        assert_eq!(format_duration(Duration::from_millis(3661 * 1000 + 500)), "1 hour 1 minute");
     }
 
     #[test]
     fn format_duration_1_hour_1_minute_2_seconds() {
-        assert_eq!(format_duration(Duration::from_secs(3662)), "1 hour 1 minute 2 seconds");
+        assert_eq!(format_duration(Duration::from_secs(3662)), "1 hour 1 minute");
     }
 
     #[test]
     fn format_duration_1_hour_2_minutes() {
-        assert_eq!(format_duration(Duration::from_secs(3720)), "1 hour 2 minutes 0 seconds");
+        assert_eq!(format_duration(Duration::from_secs(3720)), "1 hour 2 minutes");
     }
 
     #[test]
     fn format_duration_1_hour_2_minutes_1_second() {
-        assert_eq!(format_duration(Duration::from_secs(3721)), "1 hour 2 minutes 1 second");
+        assert_eq!(format_duration(Duration::from_secs(3721)), "1 hour 2 minutes");
     }
 
     #[test]
     fn format_duration_1_hour_2_minutes_2_seconds() {
-        assert_eq!(format_duration(Duration::from_secs(3722)), "1 hour 2 minutes 2 seconds");
+        assert_eq!(format_duration(Duration::from_secs(3722)), "1 hour 2 minutes");
     }
 }
